e11511921f6dacf69369fc1468f3228bbff5716e
Ayahamizu <ayacoco.1127512@gmail.com>
2024-03-05 11:57:06 +0900

    検索結果画面作成

---
+++ database/migrations/2024_03_01_015612_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('faculty_id')->constrained();
+            $table->foreignId('major_id')->constrained();
+            $table->foreignId('seminar_id')->constrained();
+            $table->text('content')->nullable();
+            $table->text('teacher')->nullable();
+            $table->text('career')->nullable();
+            $table->boolean('Isgroup')->nullable();
+            $table->boolean('Iscareer')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
